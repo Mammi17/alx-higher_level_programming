9b9f410bbabe236d3649f1ebd019450a0fae44d4
hadjara <hadjaramamane61@gmail.com>
2023-05-16 15:28:34 -0700

    task 0-14

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -2,7 +2,7 @@
 def multiple_returns(sentence):
     tuple_m = ()
     if len(sentence) == 0:
-        "None"
+       tuple_m = 0, "None"
     else:
         tuple_m = len(sentence), sentence[0]
     return tuple_m
