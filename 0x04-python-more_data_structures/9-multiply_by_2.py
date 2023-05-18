e565c005055daeeb91bf99061ae60f7abecd00f5
hadjara <hadjaramamane61@gmail.com>
2023-05-17 17:49:06 -0700

    task 0-16

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,5 +1,3 @@
 #!/usr/bin/python3
-def multiply_by_2(a_dictonary):
-    for a in a_dictionary:
-        a_dictionary[a] *= 2
-        return (a_dictionary)
+def multiply_by_2(a_dictionary):
+    return ({a: a_dictionary[a] * 2 for a in a_dictionary})
