9dbccf43db6ad9f78b56116325fe0dba14358d4c
hadjara <hadjaramamane61@gmail.com>
2023-05-17 19:06:26 -0700

    task 0-16

--- 0x04-python-more_data_structures/103-python.c
+++ 0x04-python-more_data_structures/103-python.c
@@ -5,30 +5,27 @@ void print_python_bytes(PyObject *p);
 
 /**
  * print_python_list - Prints basic info about Python lists.
- * @p: A PyObject
- * Return: void
+ * @p: A PyObject list object.
  */
 
 void print_python_list(PyObject *p)
 {
 	int taille, alloue, a;
-	const char *tpe;
-	PyListObject *l = (PyListObject *)p;
-	PyVarObject *val = (PyVarObject *)p;
+	const char *type;
+	PyListObject *list = (PyListObject *)p;
+	PyVarObject *var = (PyVarObject *)p;
 
-	taille = val->ob_size;
-	alloue = l->allocated;
+	taille = var->ob_size;
+	alloue = list->allocated;
 	printf("[*] Python list info\n");
 	printf("[*] Size of the Python List = %d\n", taille);
 	printf("[*] Allocated = %d\n", alloue);
-	a = 0
-	while (a < taille)
+	for (a = 0; a < taille; a++)
 	{
-		tpe = l->ob_item[a]->ob_type->tp_name;
-		printf("Element %d: %s\n", a, tpe);
-		if (strcmp(tpe, "bytes") == 0)
-			print_python_bytes(l->ob_item[a]);
-		a++;
+		type = list->ob_item[a]->ob_type->tp_name;
+		printf("Element %d: %s\n", a, type);
+		if (strcmp(type, "bytes") == 0)
+			print_python_bytes(list->ob_item[a]);
 	}
 }
 
@@ -40,7 +37,7 @@ void print_python_list(PyObject *p)
 void print_python_bytes(PyObject *p)
 {
 	unsigned char a, taille;
-	PyBytesObject *bte = (PyBytesObject *)p;
+	PyBytesObject *bytes = (PyBytesObject *)p;
 
 	printf("[.] bytes object info\n");
 	if (strcmp(p->ob_type->tp_name, "bytes") != 0)
@@ -49,21 +46,18 @@ void print_python_bytes(PyObject *p)
 		return;
 	}
 	printf("  size: %ld\n", ((PyVarObject *)p)->ob_size);
-	printf("  trying string: %s\n", bte->ob_sval);
+	printf("  trying string: %s\n", bytes->ob_sval);
 	if (((PyVarObject *)p)->ob_size > 10)
 		taille = 10;
 	else
 		taille = ((PyVarObject *)p)->ob_size + 1;
-
 	printf("  first %d bytes: ", taille);
-	a = 0;
-	while (a < taille)
+	for (a = 0; a < taille; a++)
 	{
-		printf("%02hhx", bte->ob_sval[a]);
+		printf("%02hhx", bytes->ob_sval[a]);
 		if (a == (taille - 1))
 			printf("\n");
 		else
 			printf(" ");
-		a++;
 	}
 }
