98971de9ca354132339c33eaedb715b45f0c488e
hadjara <hadjaramamane61@gmail.com>
2023-05-17 18:35:00 -0700

    task 0-16

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
     a_dictionary = {'M': 1000, 'D': 500, 'C': 100,
-                     'L': 50, 'X': 10, 'V': 5, 'I': 1}
+            'L': 50, 'X': 10, 'V': 5, 'I': 1}
     if roman_string is None or type(roman_string) != str:
         return 0
     count = 0
