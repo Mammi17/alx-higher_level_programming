049665931084d90aaa3483e5078f1a4a1965990c
hadjara <hadjaramamane61@gmail.com>
2023-05-29 19:25:43 -0700

    task 0-10

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -3,7 +3,7 @@
 def safe_print_division(a, b):
     try:
         count = a / b
-    except:
+    except (TypeError, ZeroDivisionError):
         count = None
     finally:
         print("Inside result: {}".format(count))
