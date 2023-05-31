b626176da59eebe670686e364a5d27312cc458a2
hadjara <hadjaramamane61@gmail.com>
2023-05-30 18:04:48 -0700

    task 0-10

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+class Square:
+"""Defines a class Square by private"""
+    """Represents a square"""
+
+    def __init__(self, size):
+        self.__size = size
---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+
+class Square:
+    """Defines a square."""
+
+    def __init__(self, size=0):
+        self.size = size
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError('size must be an integer')
+        if value < 0:
+            raise ValueError('size must be >= 0')
+        self.__size = value
+
+    def area(self):
+        return self.__size ** 2
+
+    def my_print(self):
+        for a in range(self.size):
+            for b in range(self.size):
+                print("#", end="\n" if b is self.size - 1 and a != b else "")
+        print()
