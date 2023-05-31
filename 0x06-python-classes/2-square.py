d7506ce58772313dd6d422ad02615c1f08e87968
hadjara <hadjaramamane61@gmail.com>
2023-05-30 18:55:55 -0700

    task 0-10

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 
+"""Defines a square"""
+
+
 class Square:
-    """Defines a square"""
     """represents a square."""
 
     def __init__(self, size=0):
--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 
+"""Defines a class Square."""
+
+
 class Square:
-    """Defines a class Square."""
     """Represents a square."""
 
     def __init__(self, size=0):
