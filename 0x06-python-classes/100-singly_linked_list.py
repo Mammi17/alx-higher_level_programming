bbba2138e6daf97f066816daa3af5f96e52377bd
hadjara <hadjaramamane61@gmail.com>
2023-05-30 18:35:22 -0700

    task 0-10

---
+++ 0x06-python-classes/100-singly_linked_list.py
@@ -0,0 +1,56 @@
+#!/usr/bin/python3
+class Node:
+    def __init__(self, data, next_node=None):
+        self.data = data
+        self.next_node = next_node
+
+    @property
+    def data(self):
+        return self.__data
+
+    @data.setter
+    def data(self, value):
+        if not isinstance(value, int):
+            raise TypeError('data must be an integer')
+        self.__data = value
+
+    @property
+    def next_node(self):
+        return self.__next_node
+
+    @next_node.setter
+    def next_node(self, value):
+        if value is not None and type(value) != Node:
+            raise TypeError('next_node must be a Node object')
+        self.__next_node = value
+
+
+class SinglyLinkedList:
+    def __init__(self):
+        self.head = None
+
+    def __str__(self):
+        c = ""
+        current = self.head
+        while current:
+            c += str(current.data) + "\n"
+            current = current.next_node
+        return c[:-1]
+
+    def sorted_insert(self, value):
+        nouveau = Node(value)
+        if not self.head:
+            self.head = nouveau
+            return
+
+        if value < self.head.data:
+            nouveau.next_node = self.head
+            self.head = nouveau
+            return
+
+        current = self.head
+        while current.next_node and current.next_node.data < value:
+            current = current.next_node
+        if current.next_node:
+            nouveau.next_node = current.next_node
+        current.next_node = nouveau
---
+++ 0x06-python-classes/101-square.py
@@ -0,0 +1,57 @@
+#!/usr/bin/python3
+
+class Square:
+    """Represents a square."""
+
+    def __init__(self, size=0, position=(0, 0)):
+        self.size = size
+        self.position = position
+
+    @property
+    def size(self):
+        return (self.__size)
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    @property
+    def position(self):
+        return (self.__position)
+
+    @position.setter
+    def position(self, value):
+        if (not isinstance(value, tuple) or
+                len(value) != 2 or
+                not all(isinstance(n, int) for n in value) or
+                not all(n >= 0 for n in value)):
+            raise TypeError("position must be a tuple of 2 positive integers")
+        self.__position = value
+
+    def area(self):
+        return (self.__size * self.__size)
+
+    def my_print(self):
+        if self.__size == 0:
+            print("")
+            return
+
+        [print("") for a in range(0, self.__position[1])]
+        for a in range(0, self.__size):
+            [print(" ", end="") for b in range(0, self.__position[0])]
+            [print("#", end="") for l in range(0, self.__size)]
+            print("")
+
+    def __str__(self):
+        if self.__size != 0:
+            [print("") for a in range(0, self.__position[1])]
+        for i in range(0, self.__size):
+            [print(" ", end="") for b in range(0, self.__position[0])]
+            [print("#", end="") for l in range(0, self.__size)]
+            if i != self.__size - 1:
+                print("")
+        return ("")
