a6ed69638c2c57ec1fcbe6356ee10a57345ea365
hadjara <hadjaramamane61@gmail.com>
2023-06-06 16:29:49 -0700

    task 0-10

--- 0x08-python-more_classes/8-rectangle.py
+++ 0x08-python-more_classes/8-rectangle.py
@@ -52,7 +52,7 @@ class Rectangle:
         if self.height == 0 or self.width == 0:
             return 0
         return 2 * (self.width + self.height)
-    
+
     def __str__(self):
         """Returns a string representation of the rectangle"""
 
