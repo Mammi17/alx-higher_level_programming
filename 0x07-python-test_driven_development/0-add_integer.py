30e7de2522ea9452ebb786af37b48158d873e149
hadjara <hadjaramamane61@gmail.com>
2023-06-07 19:15:10 -0700

    task 0-8

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -4,15 +4,8 @@
 
 def add_integer(a, b=98):
     """Return the addition of two numbers."""
-     tpe = [int, float]
-    error_a = "a must be an integer"
-    error_b = "b must be an integer"
-    if (type(a) not in tpe):
-        raise TypeError(error_a)
-    if (type(b) not in tpe):
-        raise TypeError(error_b)
-    if (type(a) is float):
-        a = int(a)
-    if (type(b) is float):
-        b = int(b)
-    return (a + b)
+     if ((not isinstance(a, int) and not isinstance(a, float))):
+        raise TypeError("a must be an integer")
+    if ((not isinstance(b, int) and not isinstance(b, float))):
+        raise TypeError("b must be an integer")
+    return (int(a) + int(b))
