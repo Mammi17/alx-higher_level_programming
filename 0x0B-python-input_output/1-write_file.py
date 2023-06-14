2d3c0edd4b2b50a5f640efd035142ad87190a610
hadjara <hadjaramamane61@gmail.com>
2023-06-13 18:17:21 -0700

    task 0-14

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
-"""Contains the function "wrtie_file""""
+"""Contains the function "wrtie_file"""
 
 
 def write_file(filename="", text=""):
-    """function that writes a string to a text file (UTF8) and
-    returns the number of characters written"""
+    """function that writes a string to a text file (UTF8)
+    andreturns the number of characters written"""
     with open(filename, 'w', encoding='utf=8') as f:
         return f.write(text)
