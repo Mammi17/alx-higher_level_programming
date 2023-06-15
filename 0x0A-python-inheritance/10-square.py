d9bb368865dd35d62bfa5dbe2ec523edef225256
hadjara <hadjaramamane61@gmail.com>
2023-06-14 17:31:58 -0700

    task 0-13

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -2,15 +2,20 @@
 """Contains the class BaseGeometry and subclass Rectangle"""
 
 Rectangle = __import__('9-rectangle').Rectangle
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
 class Square(Rectangle):
     """A representation of a square"""
     def __init__(self, size):
         """instantiation of the square"""
+        
         self.integer_validator("size", size)
-        self.__size = size
         super().__init__(size, size)
+        self.__size = size
+
+    def __str__(self):
+        return super().__str__()
 
     def area(self):
         """"the area of the square"""
