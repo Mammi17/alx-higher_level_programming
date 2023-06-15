f17f9cddaa1dfbd45ea78337e1de4c11ddd26b5a
hadjara <hadjaramamane61@gmail.com>
2023-06-14 17:35:20 -0700

    task 0-13

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -9,7 +9,6 @@ class Square(Rectangle):
     """A representation of a square"""
     def __init__(self, size):
         """instantiation of the square"""
-        
         self.integer_validator("size", size)
         super().__init__(size, size)
         self.__size = size
