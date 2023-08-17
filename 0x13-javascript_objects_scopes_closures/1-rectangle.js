f1fdb21a4da7909b3bf7181850554706f65b7be4
hadjara <hadjaramamane61@gmail.com>
2023-08-16 18:10:17 -0700

    task 0-13

---
+++ 0x13-javascript_objects_scopes_closures/1-rectangle.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+// A Rectangle class with w and h attributes.
+
+module.exports = class Rectangle {
+  constructor (w, h) {
+    this.width = w;
+    this.height = h;
+  }
+};
---
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+// function that returns the reversed version of a list.
+
+exports.esrever = function (list) {
+  const reversedList = [];
+  const last = list.length - 1;
+  for (let a = last; a >= 0; a--) {
+    reversedList.push(list[i]);
+  }
+  return (reversedList);
+};
