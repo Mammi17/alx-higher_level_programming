16d5344e7ba17335d11b532c77ce648b7bb1ff72
hadjara <hadjaramamane61@gmail.com>
2023-08-25 19:06:53 -0700

    task 0-17

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -11,7 +11,7 @@ if __name__ == "__main__":
         'mysql+mysqldb://{}:{}@localhost/{}'
         .format(argv[1], argv[2],
                 argv[3]), pool_pre_ping=True)
-    Base.metadata.create_all(engine)
+    Base.metadata.create_all(ngne)
     Session = sessionmaker(bind=ngne)
     n = Session()
     for state in n.query(State).order_by(State.id).all():
