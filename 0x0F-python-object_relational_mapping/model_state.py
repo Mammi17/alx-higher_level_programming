a28b2358827a534a0c12882f89841429f63643cb
hadjara <hadjaramamane61@gmail.com>
2023-08-25 18:58:21 -0700

    task 0-17

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
-"""Start link class to table in database
-"""
+"""link class to table in database"""
 from sys import argv
 from model_state import Base, State
 from sqlalchemy import (create_engine)
+
 
 if __name__ == "__main__":
     ngne = create_engine(
