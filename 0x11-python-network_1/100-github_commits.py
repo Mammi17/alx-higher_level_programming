89131f39d8d98a7a4332104fdaafe50155da8467
hadjara <hadjaramamane61@gmail.com>
2023-09-01 19:20:52 -0700

    task 0-10

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -10,14 +10,14 @@ if __name__ == '__main__':
     repo_name = sys.argv[1]
     owner_name = sys.argv[2]
 
-    url = f'https://api.github.com/repos/{owner}/{repo}/commits'
+    url = f'https://api.github.com/repos/{owner_name}/{repo_name}/commits'
     paginate = {'per_page': 10}
     res = requests.get(url, params=paginate)
 
     if res.status_code == 200:
-        commits = res.json()
+        com = res.json()
 
-        for commit in commits:
+        for commit in com:
             sha = commit['sha']
             author = commit['commit']['author']['name']
             print(f'{sha}: {author}')
