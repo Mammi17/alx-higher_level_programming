0e0ea18d4915e16b551c246d955041d1618454b9
hadjara <hadjaramamane61@gmail.com>
2023-09-05 19:09:01 -0700

    task 0-9

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # That takes in a URL, sends a GET request to the URL,
 # displays the body of the response
-curl -sfL "$1" -X GET
+curl -L -s -o /dev/null -w "%{http_code}" "$1" | grep -q 200 && curl -Ls -X GET "$1"
